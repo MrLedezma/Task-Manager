e8851a15e48c932103508cc812a6de55f58fdcb8
MrLedezma <mr.ledezma02@gmail.com>
2024-05-07 21:45:01 -0600

    implemented a task CRUD and data validation for users and tasks

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,29 @@
 import { Router } from "express";
 import { authRequierd } from "../middlewares/validateToken.js";
-
+import {
+  getTasks,
+  getTask,
+  createTask,
+  updateTasks,
+  deleteTask,
+} from "../controllers/tasks.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 const router = Router();
 
-router.get("/tasks", authRequierd, (req, res) => res.send("tasks"));
+router.get("/tasks", authRequierd, getTasks);
+
+router.get("/tasks/:id", authRequierd, getTask);
+
+router.post(
+  "/tasks",
+  authRequierd,
+  validateSchema(createTaskSchema),
+  createTask
+);
+
+router.delete("/tasks/:id", authRequierd, deleteTask);
+
+router.put("/tasks/:id", authRequierd, updateTasks);
 
 export default router;
